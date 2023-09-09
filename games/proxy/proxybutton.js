e5a99c63d0f1d95a101e83508014b869d7cc4e84
dh123i <132720362+dh123i@users.noreply.github.com>
2023-09-09 18:10:37 -0400

    proxy fix

--- games/proxy/proxybutton.js
+++ games/proxy/proxybutton.js
@@ -1,5 +1,5 @@
 // Replace 'https://example.com' with the URL you want to go to
-var desiredURL = 'https://mathfun27.soon.it';
+var desiredURL = 'http://mathfun27.soon.it';
 
 // Use the window.location.href property to navigate to the desired URL
 window.location.href = desiredURL;
